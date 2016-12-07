d16e0c8b7a8ac5ae900aab7569a68bcf416b7240
Hugo Freire <hfreire@exec.sh>
2016-12-07 18:44:00 +0100

    Removed previous existing server code that has already been moved into server.

--- app/index.js
+++
@@ -1,9 +0,0 @@
-const ImagesClient = require('google-images');
-const { Logger } = require('./utils')
-
-let client = new ImagesClient('014255597759783782513:qnxfarpo1tg', 'AIzaSyCphfV_HadM9KEMrkJOk4vK46EvHQsrf8w');
-
-client.search('Fredrik Holmen', { size: 'large' })
-	.then(function (images) {
-		Logger.info(images)
-	});
