d0309a41b0ecec5d117e065e5847d0f1a0e04e4f
Fredrik Holmen <fredrik.holmen@feedeo.com>
2016-12-07 22:28:50 +0100

    Added query parameter

--- app/index.js
+++ app/index.js
@@ -3,7 +3,9 @@ const { Logger } = require('./utils')
 
 let client = new ImagesClient('014255597759783782513:qnxfarpo1tg', 'AIzaSyCphfV_HadM9KEMrkJOk4vK46EvHQsrf8w');
 
-client.search('Fredrik Holmen', { size: 'large' })
+const query = "Keybroker"
+
+client.search(query, { size: 'large' })
 	.then(function (images) {
 		Logger.info(images)
 	});
