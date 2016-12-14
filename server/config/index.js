dd1be0532ab6598e40767cc1f160bd2d70d138ff
Hugo Freire <hfreire@exec.sh>
2016-12-14 22:49:01 +0100

    Fix wrong use of constant.

--- server/config/index.js
+++ server/config/index.js
@@ -1,12 +1,20 @@
+/*
+ * Copyright (c) 2016, Feedeo AB <hugo@feedeo.io>.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const { Logger } = require('../app/utils/index');
 
 module.exports = (function (env) {
+  let config
     switch (env) {
         case 'production':
-            const config = require('../env/production');
+          config = require('../env/production');
             break;
         case 'development':
-            const config = require('../env/development');
+          config = require('../env/development');
             break;
         default:
             Logger.error('FEEDEO_ENVIRONMENT environment variable not set');
