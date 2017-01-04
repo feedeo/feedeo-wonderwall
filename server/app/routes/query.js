7e5992c6b19a7a9bcacf62c03e6d1d6c6f0d00e0
Fredrik Holmen <fredrik.holmen@feedeo.com>
2017-01-04 17:26:42 +0100

    Added image meta data to message buld.

--- server/app/routes/query.js
+++ server/app/routes/query.js
@@ -8,60 +8,71 @@ const {Logger} = require('../utils');
 const { cx } = require('../../config');
 
 router.use(function (req, res, next) {
-    Logger.info(req.method, req.url);
-    next();
+  Logger.info(req.method, req.url);
+  next();
 });
 
 // home page route (http://localhost:8080)
 router.get('/', function (req, res) {
-    res.send('im the home page!');
+  res.send('im the home page!');
 });
 
 router.get('/query/:q', (req, res) => {
 
-    const query = req.params.q;;;;;;;;;;;;
+  const query = req.params.q;
 
-    let client = new ImagesClient(cx.cx_id, cx.cx_api);
+  let client = new ImagesClient(cx.cx_id, cx.cx_api);
 
-    client.search(query, {size: 'large'})
-        .then((images) => {
+  client.search(query, { size: 'large' })
+    .then((images) => {
 
-            if (images.length > 0) {
-                publisher.purgeQueue(function (res) {
-                    Logger.info(`${res.messageCount} messages was purged.`);;;;;;;;;;;;
+      if (images.length > 0) {
+        publisher.purgeQueue(function (res) {
+          Logger.info(`${res.messageCount} messages was purged.`);
 
-                    for (let img of images) {
-                        const tmp = {
-                            url: img.url,
-                            timeout: 15
-                        };;;;;;;;;;;;
+          for (let img of images) {
+            const tmp = {
+              url: img.url,
+              type: img.type,
+              width: img.width,
+              height: img.height,
+              size: img.size,
+              timeout: 15
+            };
 
-                        publisher.publish(tmp);
-                    }
-                })
-            }
-
-            res.json(images)
+            publisher.publish(tmp);
+          }
         })
-        .catch(error => Logger.warn(error))
+      }
+
+      res.json(images)
+    })
+    .catch(error => Logger.warn(error))
 
 });
 
+router.delete('/queue/', (req, res) => {
+
+  Logger.info("Purged queue");
+  res.send("Queue purged");
+
+})
+
 router.get('/queue/', function (req, res) {
 
-    publisher.countQueue(function (rs) {
-        Logger.info("Queue info", rs);;;;;;;;;;;;
-        res.json(res)
-    })
+  publisher.countQueue(function (rs) {
+    Logger.info("Queue info", rs);
+    res.json(res)
+  })
 });
 
 router.get('/status/', function (req, res) {
 
-    amqp.checkQueueOnConsole('keybroker', function (err, rs) {
-        if (err) console.error(err);
-        Logger.info(rs);;;;;;;;;;;;
-        res.json(rs)
-    })
+  amqp.checkQueueOnConsole('keybroker', function (err, rs) {
+    if (err) console.error(err);
+    Logger.info(rs);
+    res.json(rs)
+  })
 });
 
 module.exports = router;
