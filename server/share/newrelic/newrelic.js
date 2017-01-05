f7dcc8a6802ddfc8045bb3485c7c3b91f398b83e
Hugo Freire <hfreire@exec.sh>
2017-01-05 09:32:37 +0100

    Added NewRelic support.

---
+++ server/share/newrelic/newrelic.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017, Feedeo AB <hugo@feedeo.io>.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: [ 'Feedeo Wonderwall' ],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '039b4f4746ff6120af6bd7f741b87dec7d518082',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,13 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+const ENVIRONMENT = process.env.FEEDEO_ENVIRONMENT
+
+if (ENVIRONMENT === 'production') {
+  require('newrelic')
+}
+
 /**
  * Module dependencies.
  */
